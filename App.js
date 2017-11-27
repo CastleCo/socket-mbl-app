24e94bba4dbfc9f72b487de682f03ab2690e3620
joshpaulchan <joshpaulchan@gmail.com>
2017-11-26 23:45:10 -0500

    Async font loading for 23.0.0

--- App.js
+++ App.js
@@ -30,19 +30,19 @@ export default class App extends React.Component {
 
   async _loadAssetsAsync() {
     try {
-      await Promise.all([
-        Asset.loadAsync([
-          require('./src/assets/images/robot-dev.png'),
-          require('./src/assets/images/robot-prod.png'),
-        ]),
-        Font.loadAsync([
-          // This is the font that we are using for our tab bar
-          Ionicons.font,
-          // We include SpaceMono because we use it in HomeScreen.js. Feel free
-          // to remove this if you are not using it in your app
-          { 'space-mono': require('./src/assets/fonts/SpaceMono-Regular.ttf') },
-        ]),
-      ]);
+      // await Promise.all([
+      //   Asset.loadAsync([
+      //     require('./src/assets/images/robot-dev.png'),
+      //     require('./src/assets/images/robot-prod.png'),
+      //   ]),
+      //   Font.loadAsync([
+      //     // This is the font that we are using for our tab bar
+      //     Ionicons.font,
+      //     // We include SpaceMono because we use it in HomeScreen.js. Feel free
+      //     // to remove this if you are not using it in your app
+      //     { 'space-mono': require('./src/assets/fonts/SpaceMono-Regular.ttf') },
+      //   ]),
+      // ]);
     } catch (e) {
       // In this case, you might want to report the error to your error
       // reporting service, for example Sentry
