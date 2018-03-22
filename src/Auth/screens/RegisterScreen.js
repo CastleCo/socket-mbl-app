45aee1949afd0958acbab38df710e636a60342fd
joshpaulchan <joshpaulchan@gmail.com>
2018-03-22 12:12:00 -0400

    Move form into separate component, clean up register screen

--- src/Auth/screens/RegisterScreen.js
+++ src/Auth/screens/RegisterScreen.js
@@ -1,172 +1,83 @@
 import React from 'react';
-import { View, Image, TextInput } from 'react-native';
+import PropTypes from "prop-types";
+import { View, Image } from 'react-native';
 import {
   Container,
-  Header,
-  Title,
-  Left,
-  Right,
-  Grid,
-  Row,
-  Col,
   Content,
-  Form,
-  Item,
-  Input,
-  Label,
+  H1,
   Button,
   Text,
-  Footer,
-  Icon,
-  H1
 } from 'native-base';
+import { NavigationActions } from "react-navigation";
+import { connect } from "react-redux";
 
-import { AuthService } from '../../api';
-import { PasswordInput } from '../components';
+
+// import { AuthService } from '../../api';
+import { registerUser } from "../../actions/auth";
+import { RegisterForm } from '../components';
 
 import styles from '../../common/constants/Colors';
 const { primaryColor } = styles;
 
-export default class LoginScreen extends React.Component {
+class RegisterScreen extends React.Component {
   constructor(props) {
     super(props);
-
-    var form =  {
-      email: "",
-      password: "",
-      firstName: "",
-      lastName: "",
-    };
-
-    this.state = {
-      showPassword: false,
-      form,
-      errors: (new Map()),
-    }
-
-    // initalize auth service
-    this.auth = new AuthService("http://localhost:3000/v1");
   }
-  _togglePasswordVisibility = _ => { this.setState({ showPassword: !this.state.showPassword }); }
-  _updatePassword = (password) => {
-    this.setState({
-      form: Object.assign(this.state.form, { password })
+  componentWillReceiveProps(newProps) {
+    if (newProps.authenticated) this._goToMainScreen();
+  }
+  _goToMainScreen = () => {
+    // TODO: replace once RN-navigation is also handled by redux
+    // upon sucessful register, move them into app
+    const action = NavigationActions.reset({
+      index: 0,
+      key: null,
+      actions: [NavigationActions.navigate({ routeName: 'App' })]
     });
-  }
-  _updateEmail = (email) => {
-    this.setState({ form: Object.assign(this.state.form, { email }) });
-  }
-  _updateFirstName = (firstName) => {
-    this.setState({ form: Object.assign(this.state.form, { firstName }) });
-  }
-  _updateLastName = (lastName) => {
-    this.setState({ form: Object.assign(this.state.form, { lastName }) });
-  }
-  _submitRegister = () => {
-    // submit Register information
-    this.auth.register(this.state.form.email, this.state.form.password)
-      .then((data) => {
-        // save auth tokens
-
-        // move into profile creation page
-        const action = NavigationActions.reset({
-          index: 0,
-          key: null,
-          actions: [NavigationActions.navigate({ routeName: 'App' })]
-        });
-        this.props.navigation.dispatch(action);
-      }, (err) => {
-        this.setState((state) => {
-          const errors = new Map(state.errors);
-          // set errors on
-          errors.set('email', true);
-          errors.set('password', true);
-          return { errors };
-        })
-      })
+    this.props.navigation.dispatch(action);
   }
   _goToLoginScreen = () => {
+    // TODO: replace once RN-navigation is also handled by redux
     // navigate to Login page
-    const now = (new Date()).toString();
-    console.log(`[${now}] navigate-back screen='Login'`);
     this.props.navigation.navigate("Login");
   }
-  _focus = field => this[field]._root.focus()
   render() {
     return (
-      <Container style={{ backgroundColor: "#fff"}}>
+      <Container style={{ backgroundColor: "#fff" }}>
         <Content>
-          <View style={{ alignItems : 'center', justifyContent: 'center', height: 192, marginTop: 32 }}>
+          <View style={{ alignItems: 'center', justifyContent: 'center', height: 192, marginTop: 32 }}>
             <Image style={{ width: 128, height: 128 }} source={require('../../assets/icons/app-icon.png')} />
             <H1 style={{ fontWeight: "700", color: primaryColor }}>CASTLE</H1>
           </View>
-          <Form>
-            <Item error={this.state.errors.get('firstName')} stackedLabel>
-              <Label style={{ color: "#777" }}>First Name</Label>
-              <Input
-                placeholder="Malcolm"
-                autoCapitalize="words"
-                placeholderTextColor="#ccc"
-                returnKeyType="next"
-                value={this.state.form.firstName}
-                onChangeText={this._updateFirstName}
-                onSubmitEditing={_ => this._focus("_inputLastName") }
-                blurOnSubmit={false}
-              />
-            </Item>
-            <Item error={this.state.errors.get('lastName')} stackedLabel>
-              <Label style={{ color: "#777" }}>Last Name</Label>
-              <Input
-                ref={c => this._inputLastName = c}
-                placeholder="Gladwell"
-                autoCapitalize="words"
-                placeholderTextColor="#ccc"
-                returnKeyType="next"
-                value={this.state.form.lastName}
-                onChangeText={this._updateLastName}
-                onSubmitEditing={evt => this._focus("_inputEmail")}
-                blurOnSubmit={false}
-              />
-            </Item>
-            <Item error={this.state.errors.get('email')} stackedLabel>
-              <Label style={{ color: "#777"}}>Email</Label>
-              <Input
-                ref={c => this._inputEmail = c}
-                placeholder="your@email.com"
-                autoCapitalize="none"
-                placeholderTextColor="#ccc"
-                keyboardType="email-address"
-                returnKeyType="next"
-                value={this.state.form.email}
-                onChangeText={this._updateEmail}
-                onSubmitEditing={evt => this._focus("_inputPassword")}
-                blurOnSubmit={false}
-              />
-            </Item>
-            <Item error={this.state.errors.get('password')} stackedLabel last>
-              <Label style={{ color: "#777"}}>Password</Label>
-              <PasswordInput
-                getRef={c => this._inputPassword = c}
-                placeholder="password"
-                placeholderTextColor="#ccc"
-                value={this.state.form.password}
-                onChangeText={this._updatePassword}
-                showPassword={this.state.showPassword}
-                onTogglePassword={this._togglePasswordVisibility}
-                onSubmitEditing={(evt) => (this._submitLogin)}
-              />
-            </Item>
-          </Form>
-          <Button block primary style={{ margin: 16 }} onPress={this._submitLogin}>
-            <Text>Register</Text>
-          </Button>
+          <RegisterForm
+            onSubmit={this.props.onRegister}
+            errors={this.props.formErrors} />
         </Content>
-        <View>
-          <Button full transparent dark onPress={this._goToLoginScreen}>
-            <Text style={{ color: primaryColor }}>I already have an account</Text>
-          </Button>  
-        </View>  
+        <Button full transparent onPress={this._goToLoginScreen}>
+          <Text>I already have an account</Text>
+        </Button>
       </Container>
-    )
+    );
   }
 }
+
+RegisterScreen.propTypes = {
+  onRegister: PropTypes.func.isRequired,
+  formErrors: PropTypes.instanceOf(Map)
+};
+
+const mapStateToProps = (state) => ({
+  authenticated: state.authenticated,
+  formErrors: state.formErrors || new Map()
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  onRegister: ({email, password, firstName, lastName}) => {
+    dispatch(registerUser(email, password, firstName, lastName));
+  }
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps  
+)(RegisterScreen)
