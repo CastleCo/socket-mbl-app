0712fda4c3de063aa4b40aeecbfe5231b38917f6
joshpaulchan <joshpaulchan@gmail.com>
2018-03-28 01:06:09 -0400

    Add new screens to settings navigator

--- src/navigation/SettingsNavigator.js
+++ src/navigation/SettingsNavigator.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import { StackNavigator } from 'react-navigation';
-import { SettingsScreen } from '../Settings/screens';
+import { SettingsScreen, ExpoConfigScreen, ExpoLinksScreen } from '../Settings/screens';
+
+import { Button, Icon } from "native-base";
+import styles from '../common/constants/Colors';
+const { primaryColor } = styles;
 
 export default StackNavigator({
   Settings: {
     path: '/settings',
-    screen: SettingsScreen
+    screen: SettingsScreen,
+  },
+  Config: {
+    path: '/config',
+    screen: ExpoConfigScreen
+  },
+  Links: {
+    path: '/links',
+    screen: ExpoLinksScreen
   },
 }, {
-  headerMode: 'none'
+  initialRouteName: 'Settings',
+  navigationOptions: {
+    headerStyle: {
+      backgroundColor: primaryColor
+    },
+    headerTintColor: "#fff",
+    headerTitleStyle: {
+      fontWeight: "bold"
+    }
+  }
 });
