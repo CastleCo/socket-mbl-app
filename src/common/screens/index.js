3ea4bd0748c6e77d00a421763fbbca0ea7096e52
joshpaulchan <joshpaulchan@gmail.com>
2018-03-28 01:01:29 -0400

    Remove common screens

--- src/common/screens/index.js
+++ src/common/screens/index.js
@@ -1,5 +1,3 @@
 
-export { default as SideMenuScreen } from './SideMenuScreen';
-export { default as HomeScreen } from './HomeScreen';
-export { default as LinksScreen } from './LinksScreen';
-export { default as SettingsScreen } from './SettingsScreen';
+export { default as ExpoLinksScreen } from './ExpoLinksScreen';
+export { default as ExpoConfigScreen } from './ExpoConfigScreen';
