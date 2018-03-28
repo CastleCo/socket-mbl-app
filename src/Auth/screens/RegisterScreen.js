39aead6b431d55804ddb9f5fdc7ebfff2b7c7553
joshpaulchan <joshpaulchan@gmail.com>
2018-03-28 04:04:27 -0400

    Clean up RegisterScreen

--- src/Auth/screens/RegisterScreen.js
+++ src/Auth/screens/RegisterScreen.js
@@ -14,54 +14,33 @@ import { connect } from "react-redux";
 import { register } from "../../modules/auth/action-creators";
 import { Header, RegisterForm } from '../components';
 
-class RegisterScreen extends React.Component {
-  // componentWillReceiveProps(newProps) {
-  //   if (newProps.authenticated) this._goToMainScreen();
-  // }
-  // _goToMainScreen = () => {
-  //   // TODO: replace once RN-navigation is also handled by redux
-  //   // upon sucessful register, move them into app
-    
-  //   const action = NavigationActions.reset({
-  //     index: 0,
-  //     key: null,
-  //     actions: [
-  //       NavigationActions.navigate({ routeName: 'App' })
-  //     ]
-  //   });
-  //   this.props.navigation.dispatch(action);
-  // }
-  _goToLoginScreen = () => {
-    // navigate to Login page
-    this.props.navigation.navigate("Login");
-  }
-  render() {
-    return (
-      <Container style={{ backgroundColor: "#fff" }}>
-        <Content>
-          <Header height={192}/>
-          <RegisterForm
-            onSubmit={this.props.onRegister}
-            errors={this.props.formErrors}
-            disableSubmit={this.props.awaitingResponse}
-          />
-        </Content>
-        <Button full transparent onPress={this._goToLoginScreen}>
-          <Text>I already have an account</Text>
-        </Button>
-      </Container>
-    );
-  }
+const RegisterScreen = props => {
+  const { onRegister, formErrors, awaitingResponse, goToLoginScreen } = props;
+  return (
+    <Container style={{ backgroundColor: "#fff" }}>
+      <Content>
+        <Header height={192}/>
+        <RegisterForm
+          onSubmit={onRegister}
+          errors={formErrors}
+          disableSubmit={awaitingResponse}
+        />
+      </Content>
+      <Button full transparent onPress={goToLoginScreen}>
+        <Text>I already have an account</Text>
+      </Button>
+    </Container>
+  );
 }
 
 RegisterScreen.propTypes = {
-  authenticated: PropTypes.bool,
   formErrors: PropTypes.instanceOf(Map).isRequired,
+  awaitingResponse: PropTypes.bool.isRequired,
   onRegister: PropTypes.func.isRequired,
+  goToLoginScreen: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
-  authenticated: state.auth.authenticated,
   formErrors: state.auth.form.errors || new Map(),
   awaitingResponse: state.auth.isLoggingIn,
 })
@@ -69,7 +48,9 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch => ({
   onRegister: ({email, password, firstName, lastName}) => {
     dispatch(register(email, password, firstName, lastName));
-  }
+  },
+  goToLoginScreen: _ =>
+    dispatch(NavigationActions.navigate({ routeName: 'Login' })),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(RegisterScreen)
