a0dce6cf94078d4256d82cacb9cc106b43658531
joshpaulchan <joshpaulchan@gmail.com>
2018-03-29 23:12:09 -0400

    Implement login/register failure state transactions

--- src/modules/auth/reducer.js
+++ src/modules/auth/reducer.js
@@ -1,18 +1,19 @@
 import {
+  REGISTER,
+  REGISTER_REQUESTED,
+  REGISTER_FAILED,
   LOGIN,
   LOGIN_REQUESTED,
   LOGIN_FAILED,
   LOGOUT,
   LOGOUT_REQUESTED,
-  REGISTER,
-  REGISTER_REQUESTED,
-  REGISTER_FAILED,
 } from './action-types';
 
 const initialState = {
   user: null,
   authenticated: false,
   isLoggingIn: false,
+  isOnboarding: false,
   form: {}
 };
 
@@ -23,22 +24,43 @@ export default authReducer = function (state = initialState, action) {
     case LOGIN_REQUESTED:
       return {
         ...state,
-        isLoggingIn: true
+        isLoggingIn: true,
+      };
+    case REGISTER_FAILED:
+    case LOGIN_FAILED:
+      return {
+        ...state,
+        isLoggingIn: false,
+        form: {
+          ...state.form,
+          errors: action.errors,
+          error: action.error
+        }
       }; 
     case REGISTER:
       return {
         ...state,
         isLoggingIn: false,
-        onBoarding: true,
+        isOnboarding: true,
         authenticated: true,
-        user: action.payload.user
+        user: action.payload.user,
+        form: {
+          ...state.form,
+          errors: {},
+          error: {}
+        }
       };
     case LOGIN:
       return {
         ...state,
         isLoggingIn: false,
         authenticated: true,
-        user: action.payload.user
+        user: action.payload.user,
+        form: {
+          ...state.form,
+          errors: {},
+          error: {}
+        }
       };
     case LOGOUT:
       return {
