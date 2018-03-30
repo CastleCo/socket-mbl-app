29325cbda6cdf0f760773cdd667756a94e00f33c
joshpaulchan <joshpaulchan@gmail.com>
2018-03-29 23:31:42 -0400

    Add descriptions to auth service

--- src/api/Auth.service.js
+++ src/api/Auth.service.js
@@ -1,9 +1,7 @@
 
 const ROOT_URL = "/";
 
-// Logs a user into the application
-// 
-// @desc        Sends a thing to 
+// Logs a user into the app
 // 
 // @params      String      email       the email of the user trying to login
 // @params      String      password    the password of the user trying to login
@@ -11,34 +9,35 @@ const ROOT_URL = "/";
 //  resolves on successful credential combination
 //  rejects on incorrect combination
 export function login({ email, password }) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve({
-                user: { id: '42', email },
-                accessToken: '42',
-                refreshToken: "43"
-            });
-        }, 200);
-    });
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        user: { id: 42, email },
+        accessToken: '42',
+      });
+    }, 200);
+  });
 }
 
+// Registers a user
 // 
-// 
+// @params      String      email       the email of the user registering
+// @params      String      password    the password of the user registering 
+// @params      String      firstName   the first name of the user registering
+// @params      String      lastName    the last name of the user registering
+// @returns     Promise     
+//  resolves on successful validation and creation of user
+//  rejects on incorrect combination
 export function register({ email, password, firstName, lastName }) {
-    console.log("sending fetch");
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve({
-                user: {
-                    email, firstName, lastName
-                },
-                accessToken: "42"
-            });
-            // reject({
-            //     message: "Wrong."
-            // })
-        }, 1000);
-    });
+  console.log("sending fetch");
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        user: { id: 42, email, firstName, lastName },
+        accessToken: "42"
+      });
+    }, 1000);
+  });
 }
 
 // Checks whether user is logged in
@@ -47,9 +46,9 @@ export function register({ email, password, firstName, lastName }) {
 //  resolves if user is logged in (has authenticated access to server)
 //  rejects if user is logged out (does not have auth. access to server)
 export function isLoggedIn() {
-    return new Promise((resolve, reject) => {
-        resolve({ ok: true });
-    });
+  return new Promise((resolve, reject) => {
+    resolve({ ok: true });
+  });
 }
 
 // Logs the user out of the application
@@ -58,8 +57,8 @@ export function isLoggedIn() {
 // 
 // @returns     null
 export function logout() {
-    console.log(`[${(new Date()).toString()}] logout user=${this.user.id}`);
-    return new Promise((resolve, reject) => {
-        resolve({ ok: true });
-    });
+  console.log(`[${(new Date()).toString()}] logout user=${this.user.id}`);
+  return new Promise((resolve, reject) => {
+    resolve({ ok: true });
+  });
 }
