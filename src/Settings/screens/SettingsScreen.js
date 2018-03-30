6140d84c646037e2e1656641be9ff502e2ba6afe
joshpaulchan <joshpaulchan@gmail.com>
2018-03-29 22:59:36 -0400

    Add Icon for settings and remove showing of links screen

--- src/Settings/screens/SettingsScreen.js
+++ src/Settings/screens/SettingsScreen.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import PropTypes from "prop-types";
 import { Alert } from 'react-native';
+import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import { NavigationActions } from 'react-navigation';
 import {
   Container,
   Content,
   Button,
+  Icon,
   List,
   Separator,
   ListItem,
@@ -17,29 +20,23 @@ import { logout } from "../../modules/auth/action-creators";
 class SettingsScreen extends React.Component {
   static navigationOptions = ({ navigation }) => {
     const params = navigation.state.params || {};
-      return {
-        title: "Settings",
-        headerLeft: (
-          <Button transparent light onPress={params.openDrawer}>
-            <Icon name="menu" />
-          </Button>
-        )
-      }
+    return {
+      title: "Settings",
+      headerLeft: (
+        <Button transparent light onPress={_ => navigation.navigate('DrawerToggle')}>
+          <Icon name="menu" />
+        </Button>
+      )
     }
-  componentWillMount() {
-    // Link the openDrawer fn to the header button
-    this.props.navigation.setParams({
-      openDrawer: () => this.props.navigation.navigate('DrawerOpen')
-    });
   }
   _tapLogout = () => {
     const buttons = [
-      { text: 'Logout', onPress: this.props.logout },
-      { text: 'Stay logged in', style: 'cancel' }
+      { text: 'Yes', onPress: this.props.logout },
+      { text: 'Cancel', style: 'cancel' }
     ];
     Alert.alert(
-      'Logout?',
-      'Are you sure you want to log out of Castle?',
+      'Logout',
+      'Are you sure you want to log out?',
       buttons
     )
   }
@@ -52,7 +49,6 @@ class SettingsScreen extends React.Component {
             <ListItem onPress={this._tapLogout}><Text>Log Out</Text></ListItem>
             <Separator bordered><Text>EXPO</Text></Separator>
             <ListItem onPress={this.props.goToConfig}><Text>See Config </Text></ListItem>
-            <ListItem onPress={this.props.goToLinks}><Text>See Links</Text></ListItem>
           </List>
         </Content>
       </Container>
@@ -60,10 +56,16 @@ class SettingsScreen extends React.Component {
   }
 };
 
-const mapDispatchToProps = dispatch => ({
-  logout: _ => dispatch(logout()),
-  goToConfig: _ => dispatch(NavigationActions.navigate({ routeName: 'Config' })),
-  goToLinks: _ => dispatch(NavigationActions.navigate({ routeName: 'Links' }))
-});
+SettingsScreen.propTypes = {
+  logout: PropTypes.func.isRequired,
+  goToConfig: PropTypes.func.isRequired,
+}
+
+const mapDispatchToProps = dispatch => (
+  bindActionCreators({
+    logout: _ => logout(),
+    goToConfig: _ => NavigationActions.navigate({ routeName: 'Config' }),
+  }, dispatch)
+);
 
 export default connect(null, mapDispatchToProps)(SettingsScreen);
