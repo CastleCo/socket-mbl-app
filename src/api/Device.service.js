bdaaaa4cf0fdc177ff3b56d023fc653287667234
joshpaulchan <joshpaulchan@gmail.com>
2018-04-01 12:35:10 -0400

    Refactor device api

--- src/api/Device.service.js
+++ src/api/Device.service.js
@@ -1,37 +1,20 @@
 
-export default class Service {
-    constructor(url, accessToken) {
+const API_ROOT_URL = "/";
+const _sockets = require('./sockets.json');
 
-        this.handlers = {};
-    }
-    getPage = (page, count) => {
-        return new Promise((resolve, reject) => {
-            
-        });
-    }
-    // Emits an event with a payload
-    // 
-    // @param   { String }      evtName     the event to fire
-    // @param   { Object/null } payload     the payload
-    // @return  { Object }      server response
-    emit = (evtName, payload) => {
-        const evt = {
-            action: evtName,
-            data: payload;
-        }
-        return evt;
-    }
-    // Attaches event handler to fire on event
-    // 
-    // @param   { String }      evtName     the event to listen for
-    // @param   { Function }    handler     the handler to fire on event
-    // @return  { null }        nothing
-    // 
-    on = (evtName, handler) => {
-        if (this.handlers.hasOwnProperty(evtName)) {
-            this.handlers[evtName].push(handler);
-        } else {
-            this.handlers[evtName] = [handler];
-        }
-    };
+// Gets a page of devices from the
+export const getDevices = ({ count, offset }) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(_sockets.slice(offset, offset + count));
+    }, 200)
+  });
+}
+
+export const getDevice = ({ deviceId }) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(_sockets[deviceId]);
+    }, 200)
+  });
 }
--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,5 @@
 import * as _AuthService from './Auth.service';
+import * as _DeviceService from './Device.service';
 
 export const AuthService = _AuthService;
+export const DeviceService = _DeviceService;
