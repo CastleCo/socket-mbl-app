113cb30efa95708e8742faf289e480ccf39c46fd
joshpaulchan <joshpaulchan@gmail.com>
2018-04-13 20:12:43 -0400

    Add dark green subtitle color

--- src/common/native-base-theme/variables/castle.js
+++ src/common/native-base-theme/variables/castle.js
@@ -265,7 +265,7 @@ export default {
   titleFontfamily: platform === "ios" ? "System" : "Roboto_medium",
   titleFontSize: platform === "ios" ? 17 : 19,
   subTitleFontSize: platform === "ios" ? 12 : 14,
-  subtitleColor: platform === "ios" ? "#8e8e93" : "#FFF",
+  subtitleColor: "#187268",
 
   // New Variable
   titleFontColor: "#FFF",
