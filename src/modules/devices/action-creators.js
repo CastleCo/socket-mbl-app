4150baa1e97075b343f73f131ebf526105739c66
joshpaulchan <joshpaulchan@gmail.com>
2018-04-16 16:22:32 -0400

    Add update device feature functionality

--- src/modules/devices/action-creators.js
+++ src/modules/devices/action-creators.js
@@ -8,11 +8,14 @@ import {
   REFRESH_DEVICES,
   REQUEST_MORE_DEVICES,
   REQUEST_MORE_DEVICES_FAILED,
+
+  TRY_UPDATE_DEVICE_FEATURES,
+  UPDATE_DEVICE_FEATURES_FAILED
 } from './action-types';
 
 //// REQUESTING DEVICES
 
-const _replaceDeviceList = function (devices) {
+export const _replaceDeviceList = function (devices) {
   return { type: REPLACE_DEVICE_LIST, payload: { devices } };
 }
 
@@ -24,9 +27,10 @@ const _extendDeviceList = function (devices) {
 // 
 // @param   Number    count     number of devices to request
 // @param   Number    offset    number of devices to start loading after
+// @param   Number    householdId     Id of household to request
 // @returns Action    a `REQUES_MORE_DEVICES` action
-export const getMoreDevices = function(count, offset) {
-  return { type: REQUEST_MORE_DEVICES, payload: { count, offset } };
+export const getMoreDevices = function (count, offset, householdId) {
+  return { type: REQUEST_MORE_DEVICES, payload: { count, offset, householdId } };
 };
 
 // Execute the getMoreDevices request
@@ -37,13 +41,40 @@ export const _getMoreDevicesRequest = function* (action) {
     // emit registration action
     yield put(_extendDeviceList(resp));
   } catch (err) {
-    yield put({type: REQUEST_MORE_DEVICES_FAILED, error: err });
+    yield put({ type: REQUEST_MORE_DEVICES_FAILED, error: err });
   }
 };
 
 // Watch for getMoreDevice requests
 const _watchGetMoreDevicesRequest = function* () {
   yield takeEvery(REQUEST_MORE_DEVICES, _getMoreDevicesRequest);
+};
+
+//// UPDATING DEVICES
+
+// Starts the request to get a new page of devices
+// 
+// @param   Number    count     number of devices to request
+// @param   Number    offset    number of devices to start loading after
+// @param   Number    householdId     Id of household to request
+// @returns Action    a `REQUES_MORE_DEVICES` action
+export const updateDeviceFeatures = function (deviceId, features) {
+  return { type: TRY_UPDATE_DEVICE_FEATURES, payload: { deviceId, features } };
+};
+
+// Execute the getMoreDevices request
+export const _tryUpdateDeviceFeatures = function* (action) {
+  try {
+    // send registration request
+    const resp = yield call(DeviceService.updateDeviceFeatures, action.payload);
+  } catch (err) {
+    yield put({ type: UPDATE_DEVICE_FEATURES_FAILED, error: err });
+  }
+};
+
+// Watch for getMoreDevice requests
+const _watchForTryUpdateDeviceFeatures = function* () {
+  yield takeEvery(TRY_UPDATE_DEVICE_FEATURES, _tryUpdateDeviceFeatures);
 };
 
 export default function* deviceSagas() {
